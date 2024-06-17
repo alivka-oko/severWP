ae8971847de87ec2e77eb741040986125a2cbeea
alivka-oko <alina.okotnikova2002@gmail.com>
2024-06-17 16:54:30 +0300

    экология

---
+++ wp-content/themes/blankslate/ecology.php
@@ -0,0 +1,52 @@
+<?php /* Template Name: Экологичность */ ?>
+<?php get_header(); ?>
+<main class="main eco">
+    <section class="section p-96-40">
+        <div class="content">
+            <?= get_template_part('breadcrumb') ?>
+        </div>
+    </section>
+    <section class="section p-0-140 eco-1">
+        <div class="content">
+            <div class="top-content">
+                <h1 class="headline-2">Экологичность</h1>
+                <p class="text-1">Минимизация отходов, оптимизация ресурсов и снижение вредного воздействия на окружающую среду — вот основные принципы, на которых строится наша деятельность.
+
+                    Мы придаём большое значение эффективному использованию ресурсов, стремясь к максимальной экономии материалов и энергии в процессе производства. При этом мы активно внедряем инновационные технологии и методы работы, направленные на уменьшение негативного воздействия на окружающую среду.</p>
+            </div>
+            <div class="banner" style="background:url(http://sever/wp-content/uploads/2024/06/img1.png)"></div>
+        </div>
+    </section>
+    <section class="section useful-initiatives p-0-140">
+        <div class="content">
+            <div class="left-side">
+                <h2 class="headline-2  wow animate__ animate__fadeInUp animated" style="visibility: visible;">полезные инициативы</h2>
+                <div class="main-part">
+                    <div class="medium-image" style="background:url(http://sever/wp-content/uploads/2024/06/img2.png)"></div>
+                    <div class="text-block">
+                        <h3 class="headline-3"> Думаем о покупателях </h3>
+                        <p class="text-1"> Мы&nbsp;предлагаем широкий ассортимент древесины различных пород и&nbsp;размеров, чтобы&nbsp;удовлетворить потребности наших клиентов в&nbsp;различных отраслях. <br>
+                            <br>
+                            Нашему опыту и&nbsp;профессионализму доверяют, мы&nbsp;обеспечиваем&nbsp;партнёров качественной и&nbsp;сертифицированной продукцией.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="right-side">
+                <div class="mini-block wow animate__ animate__fadeInLeft animated" style="visibility: visible;"><span class="headline-3">&gt;</span><span class="headline-2">1000</span><span class="text-2">довольных
+                        клиентов</span></div>
+                <div class="large-image" style="background:url(http://sever/wp-content/uploads/2024/06/img1.png)"></div>
+            </div>
+        </div>
+    </section>
+    <section class="section care p-140" style="background: url(http://sever/wp-content/uploads/2024/06/our_mission.png);">
+        <div class="content">
+            <div class="card  wow animate__ animate__fadeInDown animated" style="visibility: visible;">
+                <img src="http://sever/wp-content/uploads/2024/06/icon.svg" alt="логотип">
+                <h2 class="headline-2 wow animate__ animate__fadeIn" style="visibility: visible;">Наша миссия</h2>
+                <p class="text-1">Не&nbsp;только производить качественную продукцию, но&nbsp;и&nbsp;сохранять богатство лесов для&nbsp;будущих поколений.</p>
+            </div>
+        </div>
+    </section>
+
+    <?php get_footer(); ?>
